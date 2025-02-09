45e84bd0a8b87b6439f3020f566b88a01d74aeab
Salman <salmanshahzad91@gmail.com>
2025-02-10 00:40:57 +0500

    Fourth Tutorial

---
+++ Type Conversion/exercise.py
@@ -0,0 +1,4 @@
+weight_pounds = input("Please Enter Your Weight(Pounds): ")
+print("Weight in pounds: ",weight_pounds)
+weight_kilograms = int(weight_pounds) / 2.205
+print("Weight in kilograms",weight_kilograms)
